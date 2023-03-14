914167dcd35fb50e2f12e968115ba9e1edae3c65
sash39 <aleksandr.egorkin@gmail.com>
2023-03-14 20:30:34 +0200

    Повторно добавил БД

--- sprintpereval/sprintpereval/settings.py
+++ sprintpereval/sprintpereval/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,8 +79,8 @@ DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'postgres',
-    'USER': 'baeldung',
-    'PASSWORD': 'baeldung',
+    'USER': 'postgres',
+    'PASSWORD': 'etfh46VN',
     'HOST': 'localhost',
     'PORT': '5432',
     }
